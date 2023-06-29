227ddbbf35a669c828a6af49a4f70f78ec198925
aryankakadiya81 <aryankakadiya81@gmail.com>
2023-06-29 14:18:11 +0530

    Create_row_in_table_via_form

--- src/Form/Ui_Form_Project/Main_ui.jsx
+++ src/Form/Ui_Form_Project/Main_ui.jsx
@@ -34,56 +34,68 @@ const Main_ui = () => {
     let [sEmail, setsEmail] = useState(Email);
     let [sLocation, setsLocation] = useState(Location);
     let [sPhone, setsPhone] = useState(Phone);
-
+    
     function Fsubmit() {
         setsName(sName)
         setsEmail(sEmail)
         setsLocation(sLocation)
         setsPhone(sPhone)
     }
-
+    
     function Delete(id1) {
-
-        setData1(Data1.filter((ele) => { return (ele.id !== id1) }));
-        // alert("Delete 1 product") ;
-
+        
+        setData1(Data1.filter((ele) => { return (ele.Email !== id1) }));
+        
     }
     function Edit(id1) {
-
+        
         setData1(Data1.filter((ele) => {
-            if (ele.id === id1) {
-
+            if (ele.Name === id1) {
+                
                 ele.Name = Name;
                 ele.Email = Email;
                 ele.Phone = Phone;
                 ele.Location = Location;
-
+                
             }
             return 1;
         }))
     }
+    let [NewData, setNewData] = useState(Data)
     function Create() {
-        setData1(Data)
+        // setData1(Data)
+        setNewData([{
+            "id":6,
+            "Name": `${ Name }`,
+            "Email": `${ Email }`,
+            "Location": `${ Location }`,
+            "Phone":  Phone 
+        },...Data1])
+        setData1(NewData)
     }
-    return (
-        <>
-            <div className="min-h-screen flex flex-col justify-center">
-                <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-
-                    <div className="relative px-4 py-10 bg-black shadow-lg sm:rounded-3xl sm:p-20">
+    // function Clearlist()
+    // {
+        //     setData1([])
+        // }
+        return (
+            <>
+            <div className="min-h-screen flex justify-center">
+            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+            
+                    <div className="relative px-4 py-10 bg-slate-300 shadow-2xl sm:rounded-3xl sm:p-20">
                         <div className="mb-4">
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline focus:shadow-outline" id="Name" type="text" placeholder="Name" value={Name} onChange={Fname} />
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Name" type="text" placeholder="Name" value={Name} onChange={Fname} />
                         </div>
-
+                        
                         <div className="mb-4">
-                            <input
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="Email"
-                                type="email"
-                                placeholder="Email"
-                                value={Email}
-                                onChange={Femail}
-                            />
+                        <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="Email"
+                        type="email"
+                        placeholder="Email"
+                        value={Email}
+                        onChange={Femail}
+                        />
                         </div>
                         <div className="mb-4">
 
@@ -109,7 +121,7 @@ const Main_ui = () => {
                         </div>
 
 
-                        <button className="bg-blue-500 text-white rounded-md px-2 py-1" onClick={Fsubmit}>Submit</button>
+                        <button className="bg-green-500 text-white rounded-md px-2 py-1" onClick={Fsubmit}>Submit</button>
                     </div>
                 </div>
             </div>
@@ -118,9 +130,8 @@ const Main_ui = () => {
 
 
 
-            <div className='container flex justify-center select-none mt-5'>
-                <div>
-                    <div className='text-center text-4xl font-bold'>Aryan's UI</div>
+            <div className='container flex justify-center select-none mt-5 '>
+                <div className='px-6'>
                     <div className="bg-white rounded-md">
 
                         <h2 className="text-black font-bold text-3xl">Products</h2>
@@ -159,20 +170,23 @@ const Main_ui = () => {
 
                                         {Data1.map((el) => {
                                             return (
-                                                <Rowadd Name={el.Name} Email={el.Email} Location={el.Location} Phone={el.Phone} Delete={() => { return Delete(el.id) }} Edit={() => { return (Edit(el.id)) }}></Rowadd>
+                                                <Rowadd Name={el.Name} Email={el.Email} Location={el.Location} Phone={el.Phone} Delete={() => { return Delete(el.Email) }} Edit={() => { return (Edit(el.Name)) }}></Rowadd>
                                             )
                                         })
                                         }
                                     </tbody>
                                 </table>
                             </div>
-                        </div>}
-                    </div>
-                </div>
-
-            </div>
-        </>
-    )
-}
-
-export default Main_ui
+                            </div>
+                        }
+                        </div>
+                        </div>
+                        
+                        </div>
+                        </>
+                        )
+                    }
+                    
+                    export default Main_ui
+                    
+                    // <button className='bg-red-700 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer ms-2' onClick={Clearlist}>Clear ALL</button>
